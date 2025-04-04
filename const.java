6a043e272a0baf7f78bf73590b782b66607bbdd6
Anish_Mulla23 <annyaa@gmail.com>
2025-04-04 09:06:42 +0530

    save

--- const.java
+++ const.java
@@ -2,13 +2,16 @@ package constructor;
 public class const{
     String model;
     int year;
-    const(String m,int y) {
+    const(String m,int y,int n) {
         model = m;
         year = y;
+        num = n;
 }
 void displayCarDetails() {
     System.out.println("Const Model: "+model);
     System.out.println("Const Year: "+year);
+    System.out.println("Const num: "+num);
+
 }
 public static void main(String[]args) {
     const c = new const("toyota",20);
